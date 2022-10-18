7e53a1f23608bcd4dab71b294ecef2ca3db6ec34
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2022-10-18 12:05:32 +0200

    Time: 8 ms (13.85%), Space: 8 MB (5.32%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int reverse(int x) {
+        //Store digits in a stack
+        //Pop them out of the stack and multiply them by 10^(size of stack)
+        //  While doing that check if outside the 32 bit integer range
+        //If negative set a flag and change it afterwards
+        
+        std::stack<int> sd;
+        std::stack<int> sdReverse;
+        
+        int positive = 1;
+        
+        if (x < 0){
+            positive = -1;
+            
+            if(x == -2147483648){
+                return 0;
+            }
+            
+            x *= -1;
+        }
+        
+        while (x > 0){
+            int digit = x%10;
+            x /= 10;
+            sd.push(digit);
+        }
+        
+        while(sd.size() != 0){
+            sdReverse.push(sd.top());
+            sd.pop();
+        }
+        
+        
+        double sum = 0;
+        while(sdReverse.size() != 0){
+            double factor = std::pow(10, sdReverse.size()-1);
+            double res = sdReverse.top()*factor;
+                
+            sum += res;
+            
+            sdReverse.pop();
+        }
+        
+        
+        if (sum >= std::pow(2, 31)){
+            return 0;
+        }
+        
+        std::cout << sum << std::endl;
+        
+        sum *= positive;
+        std::cout << sum << std::endl;
+
+        return (int)sum;
+    }
+};
