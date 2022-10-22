a35c7bd616552bcbad2696d47b3fc7f9228ff674
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2022-10-22 16:08:33 +0200

    Time: 11 ms (6.09%), Space: 8.8 MB (73.53%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -6,8 +6,6 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int k = nums.size();
         for (int i=0; i<nums.size(); i++){
-            std::cout << nums[i] <<std::endl;
-
             if(nums[i] == val){
                 for(int j = i; j<nums.size()-1; j++){
                     nums[j] = nums[j+1];
@@ -16,12 +14,6 @@ public:
                 k--;
                 i--;
             }
-            
-            for (int x = 0; x<nums.size(); x++){
-                std::cout << nums[x] << " ";
-            }
-            std::cout << std::endl;
-            
         }
         
         return k;
