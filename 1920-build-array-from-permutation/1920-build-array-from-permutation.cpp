c02779bc9de166b9cba2e616a432594b90b8c48a
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2022-10-26 15:45:53 +0200

    Time: 40 ms (21.43%), Space: 16.3 MB (51.01%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> ans(size);
+        
+        for(int i=0; i<size; i++){
+            ans[i]=nums[nums[i]];
+        }
+        
+        return ans;
+    }
+};
