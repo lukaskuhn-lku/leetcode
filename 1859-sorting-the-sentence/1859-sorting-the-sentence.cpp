fdc72bd47397a8f19d0508176eb6ae9607951715
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2022-11-19 10:15:53 +0100

    Time: 2 ms (50.15%), Space: 6.4 MB (36.74%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        std::unordered_map<int, string> words;
+        
+        int wordStart = 0;
+        int wordEnd = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == ' '){
+                wordEnd = i-1;
+                char idx = s[i-1];
+                int index = idx - '0';
+                
+                std::string sub = s.substr(wordStart, wordEnd-wordStart);
+                words[index] = sub;
+                
+                std::cout << sub << std::endl;
+                                
+                std::cout << wordStart << "|" << wordEnd << std::endl;
+                wordStart = i+1;
+            }
+        }
+        
+        //Add last word
+        wordEnd = s.size();
+        char idx = s[wordEnd-1];
+        int index = idx - '0';
+
+        std::string sub = s.substr(wordStart, wordEnd-1-wordStart);
+        words[index] = sub;
+        
+     
+        string result = "";
+        for(int i=1; i<words.size(); i++){
+            result += words[i] + " ";
+        }
+        
+        result += words[words.size()];
+        
+        return result;
+    }
+};
