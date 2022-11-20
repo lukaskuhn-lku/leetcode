5c94346ab8ebe149eb5c3e005a5884315543b85f
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2022-11-20 10:13:51 +0100

    Time: 5 ms (15.59%), Space: 6 MB (61.05%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -3,8 +3,7 @@ public:
     vector<double> convertTemperature(double celsius) {
         vector<double> ans;
         
-        double kelvin = 273.1500000L;
-        ans.push_back(celsius + kelvin);
+        ans.push_back(celsius + 273.1500000L);
         ans.push_back(celsius * 1.800000L + 32.000000L);
         
         return ans;
