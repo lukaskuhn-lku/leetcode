62a41983ab671e811b2aabc24964eb050fb0a612
Lukas Kuhn <lukaskuhn.lku@gmail.com>
2022-12-03 11:52:01 +0100

    Time: 37 ms (22.04%), Space: 8.4 MB (78.73%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        std::unordered_map<char, int> letters;
+        
+        for(char c: s){
+            letters[c] += 1;
+        }
+        
+        std::string res;        
+        
+        std::vector<std::pair<char,int>> values(letters.begin(),letters.end());
+        auto cmp = [](const std::pair<char,int>& l, std::pair<char,int>& r) { return l.second > r.second;};
+        std::sort(values.begin(),values.end(),cmp);
+        
+        
+        for(std::pair<char,int> p: values){
+            std::cout << p.first << p.second << std::endl;   
+            
+            for(int k = 0; k<p.second; k++){
+                res += p.first;
+            }
+        }
+        
+        return res;
+    }
+};
